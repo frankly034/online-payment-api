d7769253eae5bdf4e04471874ac6b6828d60acf4
frankly034 <franklynugege@gmail.com>
2023-02-13 16:58:54 +0100

    ch: add error handler

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,25 @@
-import bodyParser from "body-parser";
-import express, { Application, Request, Response } from "express";
+import bodyParser from 'body-parser';
+import express, { Application, Request, Response } from 'express';
+import { NotFoundError } from './utils/ApiError';
+import { ErrorHandler } from './utils/middlewares/ErrorHandler';
 
 const app: Application = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req: Request, res: Response) => {
+app.get('/', (req: Request, res: Response) => {
   res.status(200).json({
-    message: "hello world!",
+    message: 'hello world!',
   });
 });
 
+app.use((req: Request) => {
+  throw new NotFoundError(req.path);
+});
+
+app.use(ErrorHandler.handle);
+
 app.listen(3000, () => {
-  console.log("Server running on port 3000");
+  console.log('Server running on port 3000');
 });
